0a6ce0cea84a3c0fad0e88c5f9f5945d9b04a3dc
Stevie <xxlennygamerxx@gmail.com>
2021-04-10 02:04:49 -0700

    ping/pong command (latency)

---
+++ Core/Content/Commands/InfoCommands/PingCommand.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using TomatBot.Core.Content.Embeds;
+using TomatBot.Core.Framework.CommandFramework;
+using TomatBot.Core.Framework.DataStructures;
+
+namespace TomatBot.Core.Content.Commands.InfoCommands
+{
+    public sealed class PingCommand : TomatCommand
+    {
+        public override MethodInfo? AssociatedMethod => GetType().GetMethod("HandleCommand");
+
+        public override HelpCommandData HelpData => new HelpCommandData("ping", "Shows bot gateway latency and response time.");
+
+        public override CommandType CType => CommandType.Info;
+
+        [Command("ping")]
+        [Alias("pong")] // we do a little bit of trolling
+        [Summary("Shows bot latency.")]
+        [RequireBotPermission(ChannelPermission.SendMessages)]
+        public async Task HandleCommand()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            IUserMessage? toDelete = await ReplyAsync("Calculating...");
+            stopwatch.Stop();
+
+            await toDelete.DeleteAsync();
+
+            BaseEmbed embed = new BaseEmbed(Context.User)
+            {
+                Title = "Ping",
+
+                Description = $"Latency - `{Context.Client.Latency}ms`" +
+                              $"\nResponse Time - `{stopwatch.ElapsedMilliseconds}ms`"
+            };
+
+            await ReplyAsync(embed: embed.Build());
+        }
+    }
+}
