f87680a6698b6a7097f23183fe781af574f28eba
TheStachelfisch <benbeschow@hotmail.com>
2021-04-10 23:49:44 +0200

    Add UpdateCommand.cs and upgrade to C#9 and .Net 5

---
+++ Core/Content/Commands/OwnerCommands/UpdateCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Threading.Tasks;
+using Discord.Commands;
+using TomatBot.Core.Framework.CommandFramework;
+using TomatBot.Core.Framework.DataStructures;
+
+namespace TomatBot.Core.Content.Commands.OwnerCommands
+{
+    public class UpdateCommand : TomatCommand
+    {
+        //TODO: Change this Stevie, I don't know how your Framework works
+        public override MethodInfo? AssociatedMethod { get; }
+        public override HelpCommandData HelpData { get; }
+        public override CommandType CType { get; }
+
+        [Command()]
+        public async Task UpdateCommandAsync()
+        {
+            
+        }
+    }
+}
