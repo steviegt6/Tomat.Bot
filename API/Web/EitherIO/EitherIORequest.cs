b3046db430abc94a5f8ba327656a93e6fa91a5ab
Stevie <xxlennygamerxx@gmail.com>
2021-04-11 13:19:15 -0700

    make eitheriorequest readonly

--- API/Web/EitherIO/EitherIORequest.cs
+++ API/Web/EitherIO/EitherIORequest.cs
@@ -4,23 +4,23 @@ using HtmlAgilityPack;
 
 namespace TomatBot.API.Web.EitherIO
 {
-    public struct EitherIORequest
+    public readonly struct EitherIORequest
     {
         public readonly string optionOne;
         public readonly string optionTwo;
-        public string? exception;
+        public readonly string exception;
 
         public EitherIORequest(string optionOne, string optionTwo)
         {
             this.optionOne = optionOne;
             this.optionTwo = optionTwo;
-            exception = null;
+            exception = "";
         }
 
-        public EitherIORequest AttachException(string exception)
+        public EitherIORequest(string exception)
         {
+            optionOne = optionTwo = "";
             this.exception = exception;
-            return this;
         }
 
         public static EitherIORequest MakeRequest()
@@ -29,14 +29,14 @@ namespace TomatBot.API.Web.EitherIO
             HtmlDocument? htmlDoc = webClient.Load(@"http://www.either.io/");
 
             if (htmlDoc == null)
-                return new EitherIORequest().AttachException("HTML document was null!");
+                return new EitherIORequest("HTML document was null!");
 
             List<HtmlNode> classes = htmlDoc.DocumentNode.Descendants("span")
                 .Where(x => x.Attributes.Contains("class") && x.Attributes["class"].Value.Contains("option-text"))
                 .ToList();
 
             return classes.Count < 2
-                ? new EitherIORequest().AttachException("Could not find a minimum of two classes!")
+                ? new EitherIORequest("Could not find a minimum of two classes!")
                 : new EitherIORequest(classes[0].InnerText, classes[1].InnerText);
         }
     }
--- Core/Content/Commands/FunCommands/EitherCommand.cs
+++ Core/Content/Commands/FunCommands/EitherCommand.cs
@@ -26,7 +26,7 @@ namespace TomatBot.Core.Content.Commands.FunCommands
         {
             EitherIORequest options = EitherIORequest.MakeRequest();
 
-            if (options.exception != null)
+            if (string.IsNullOrEmpty(options.exception))
             {
                 Embed embed = EmbedHelper.ErrorEmbed(
                     "Error while attempting to make a request to [either.io](http://either.io/). If the website is up and this issue persists, contact a developer." +
