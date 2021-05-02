358951fe7119da8ce6341f7e88b3c290823e97d4
Hoovier <javierp2013@gmail.com>
2021-05-01 20:47:46 -0700

    Changed command to fit requested changes from reviewer. Made command only take one argument, added command aliases.

--- Core/Content/Commands/FunCommands/ConvertTempCommand.cs
+++ Core/Content/Commands/FunCommands/ConvertTempCommand.cs
@@ -20,42 +20,31 @@ namespace TomatBot.Core.Content.Commands.FunCommands
 
         public override CommandType CType => CommandType.Fun;
 
-        public override string Parameters => "<celsius/farenheit> <temperature>";
+        public override string Parameters => "<temperature>";
 
-        [Command("convert")]
-        [Alias("conv")]
+        [Command("converttemp")]
+        [Alias("convtemp","tempconv", "tempconvert")]
         [Summary("Converts temperatures!")]
         [RequireBotPermission(ChannelPermission.SendMessages)]
         public async Task HandleCommand(
-            string unit,
             string temp = "")
         {
             try
             {
                 int tempNumber;
-                double convertedTemp;
+                double inCelsius, inFahrenheit;
                 string response;
+                //if the number cannot be read, throw error.
                 if(!int.TryParse(temp, out tempNumber))
                 {
                     throw new InvalidOperationException("Could not read the temperature value!");
                 }
-                //celsius to fahrenheit
-                if(unit == "c" || unit == "celsius")
-                {
-                    convertedTemp = (tempNumber * 9) / 5 + 32;
-                    response = temp + " celsius is equal to " + convertedTemp + " fahrenheit!"; 
-                }
-                //fahrenheit to celsius
-                if (unit == "f" || unit == "fahrenheit")
-                {
-                    convertedTemp = ((tempNumber - 32) * 5) / 9;
-                    response = temp + " fahrenheit is equal to " + convertedTemp + " celsius!";
-                }
-                //throw exception if the checks fail
-                else
-                {
-                    throw new InvalidOperationException("Could not read the temperature value or unit, try writing celsius or c!");
-                }
+                //convert user input into both units of temperature
+                inFahrenheit = (tempNumber * 9) / 5 + 32;
+                inCelsius = ((tempNumber - 32) * 5) / 9;
+                //construct response.
+                response = temp + " celsius is equal to " + inFahrenheit + " fahrenheit!\n" +
+                temp + " fahrenheit is equal to " + inCelsius + " celsius!"; 
 
                 await ReplyAsync(response, embed: CreateSmallEmbed().Build());
             }
