75d0d8cf2b4db41acfb35b559aa05b435faa6ad3
Stevie <xxlennygamerxx@gmail.com>
2021-05-04 19:59:48 -0700

    rankine, dunno if the conversion is accurate

--- Core/Content/Commands/FunCommands/ConvertTempCommand.cs
+++ Core/Content/Commands/FunCommands/ConvertTempCommand.cs
@@ -16,7 +16,8 @@ namespace TomatBot.Core.Content.Commands.FunCommands
         {
             Fahrenheit,
             Celsius,
-            Kelvin
+            Kelvin,
+            Rankine
         }
 
         public override MethodInfo? AssociatedMethod => GetType().GetMethod("HandleCommand");
@@ -40,56 +41,54 @@ namespace TomatBot.Core.Content.Commands.FunCommands
             if (temp.EndsWith("F", StringComparison.OrdinalIgnoreCase) ||
                 temp.EndsWith("Fahrenheit", StringComparison.OrdinalIgnoreCase))
             {
-                double toF = ToFahrenheit(numbers, Unit.Fahrenheit);
-                double toC = ToCelsius(numbers, Unit.Fahrenheit);
-                double toK = ToKelvin(numbers, Unit.Fahrenheit);
-
                 await ReplyAsync(embed: new EmbedBuilder
                 {
-                    Title = "Converted Celsius",
-                    Description = $"{numbers}°F = {toF}°F" +
-                                  $"\n{numbers}°F = {toC}°C" +
-                                  $"\n{numbers}°F = {toK}°K",
+                    Title = "Converted Fahrenheit",
+                    Description = GetText(numbers, "°F", Unit.Fahrenheit),
                     Color = Color.DarkBlue
                 }.Build());
             }
             else if (temp.EndsWith("C", StringComparison.OrdinalIgnoreCase) ||
                      temp.EndsWith("Celsius", StringComparison.OrdinalIgnoreCase))
             {
-                double toF = ToFahrenheit(numbers, Unit.Celsius);
-                double toC = ToCelsius(numbers, Unit.Celsius);
-                double toK = ToKelvin(numbers, Unit.Celsius);
-
                 await ReplyAsync(embed: new EmbedBuilder
                 {
                     Title = "Converted Celsius",
-                    Description = $"{numbers}°C = {toF}°F" +
-                                  $"\n{numbers}°C = {toC}°C" +
-                                  $"\n{numbers}°C = {toK}°K",
+                    Description = GetText(numbers, "°C", Unit.Celsius),
                     Color = Color.DarkBlue
                 }.Build());
             }
             else if (temp.EndsWith("K", StringComparison.OrdinalIgnoreCase) ||
                      temp.EndsWith("Kelvin", StringComparison.OrdinalIgnoreCase))
             {
-                double toF = ToFahrenheit(numbers, Unit.Kelvin);
-                double toC = ToCelsius(numbers, Unit.Kelvin);
-                double toK = ToKelvin(numbers, Unit.Kelvin);
-
                 await ReplyAsync(embed: new EmbedBuilder
                 {
-                    Title = "Converted Celsius",
-                    Description = $"{numbers}°K = {toF}°F" +
-                                  $"\n{numbers}°K = {toC}°C" +
-                                  $"\n{numbers}°K = {toK}°K",
+                    Title = "Converted Kelvin",
+                    Description = GetText(numbers, "K", Unit.Kelvin),
+                    Color = Color.DarkBlue
+                }.Build());
+            }
+            else if (temp.EndsWith("R", StringComparison.OrdinalIgnoreCase) ||
+                     temp.EndsWith("Rankine", StringComparison.OrdinalIgnoreCase))
+            {
+                await ReplyAsync(embed: new EmbedBuilder
+                {
+                    Title = "Converted Rankine",
+                    Description = GetText(numbers, "°R", Unit.Rankine),
                     Color = Color.DarkBlue
                 }.Build());
             }
             // If its any other unit than Celsius or Fahrenheit
             else
                 await ReplyAsync(
-                    embed: EmbedHelper.ErrorEmbed("Please specify a unit (F, C, K, Fahrenheit, Celsius, Kelvin)."));
+                    embed: EmbedHelper.ErrorEmbed("Please specify a unit (F, C, K, R, Fahrenheit, Celsius, Kelvin, Rankine)."));
         }
+
+        private static string GetText(double temp, string the, Unit unit) =>
+            $"{temp}{the} = {ToFahrenheit(temp, unit)}°F" +
+            $"\n{temp}{the} = {ToCelsius(temp, unit)}°C" +
+            $"\n{temp}{the} = {ToKelvin(temp, unit)}°K," +
+            $"\n{temp}{the} = {ToRankine(temp, unit)}°R";
 
         public static double ToFahrenheit(double temp, Unit fromUnit)
         {
@@ -97,7 +96,8 @@ namespace TomatBot.Core.Content.Commands.FunCommands
             {
                 Unit.Fahrenheit => temp,
                 Unit.Celsius => Math.Round(temp * 9 / 5 + 32, 2),
-                Unit.Kelvin => Math.Round((temp - 273.15) * 1.8 + 32),
+                Unit.Kelvin => Math.Round((temp - 273.15) * 1.8 + 32, 2),
+                Unit.Rankine => temp - 491.67 + 32,
                 _ => throw new ArgumentOutOfRangeException(nameof(fromUnit), fromUnit, null)
             };
         }
@@ -106,9 +106,10 @@ namespace TomatBot.Core.Content.Commands.FunCommands
         {
             return fromUnit switch
             {
-                Unit.Fahrenheit => Math.Round((temp - 32) / 1.8),
+                Unit.Fahrenheit => Math.Round((temp - 32) / 1.8, 2),
                 Unit.Celsius => temp,
                 Unit.Kelvin => temp + 273.15,
+                Unit.Rankine => Math.Round((temp - 491.67) / 1.8, 2),
                 _ => throw new ArgumentOutOfRangeException(nameof(fromUnit), fromUnit, null)
             };
         }
@@ -117,9 +118,22 @@ namespace TomatBot.Core.Content.Commands.FunCommands
         {
             return fromUnit switch
             {
-                Unit.Fahrenheit => Math.Round((temp - 32) / 1.8 + 273.15),
+                Unit.Fahrenheit => Math.Round((temp - 32) / 1.8 + 273.15, 2),
                 Unit.Celsius => temp - 273.15,
                 Unit.Kelvin => temp,
+                Unit.Rankine => Math.Round((temp - 491.67) / 1.8 + 273.15, 2),
+                _ => throw new ArgumentOutOfRangeException(nameof(fromUnit), fromUnit, null)
+            };
+        }
+
+        public static double ToRankine(double temp, Unit fromUnit)
+        {
+            return fromUnit switch
+            {
+                Unit.Fahrenheit => temp - 32 + 491.67,
+                Unit.Celsius => Math.Round(temp * 1.8 + 491.67, 2),
+                Unit.Kelvin => Math.Round((temp - 273.15) * 1.8, 2) + 491.67,
+                Unit.Rankine => temp,
                 _ => throw new ArgumentOutOfRangeException(nameof(fromUnit), fromUnit, null)
             };
         }
