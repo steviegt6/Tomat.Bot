b1b3a179551da202b0cf1461da58df94d6d69785
Stevie <xxlennygamerxx@gmail.com>
2021-05-11 14:47:28 -0700

    sugg

---
+++ Tomat.TomatBot/Content/Commands/FunCommands/SuggestionCommand.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Tomat.CommandFramework;
+using Tomat.CommandFramework.HelpSystem;
+using Tomat.Conveniency.Embeds;
+
+namespace Tomat.TomatBot.Content.Commands.FunCommands
+{
+    public sealed class SuggestionCommand : BaseCommand
+    {
+        public override MethodInfo? AssociatedMethod => GetType().GetMethod("HandleCommand");
+
+        public override HelpCommandData HelpData =>
+            new("suggestion", "Creates a suggestion embed with an +1, -1, and indifferent option.");
+
+        public override CommandType CType => CommandType.Fun;
+
+        [Command("suggestion")]
+        [Alias("suggest")]
+        [Summary("Creates a suggestion embed.")]
+        [RequireBotPermission(ChannelPermission.SendMessages)]
+        [RequireBotPermission(ChannelPermission.AddReactions)]
+        [RequireBotPermission(ChannelPermission.ManageMessages)]
+        public async Task HandleCommand([Remainder] string suggestion)
+        {
+            await Context.Message.DeleteAsync();
+
+            EmbedBuilder realEmbed = new BaseEmbed(Context.User)
+            {
+                Title = "Suggestion!",
+                Color = Color.Gold,
+                Description = suggestion
+            };
+
+            IUserMessage? message = await ReplyAsync(embed: realEmbed.Build());
+            await message.AddReactionsAsync(new IEmote[] {new Emoji("👍"), new Emoji("👎"), new Emoji("❗")});
+        }
+
+        private static readonly string[] Websites =
+        {
+            "https://longdogechallenge.com/",
+            "http://heeeeeeeey.com/",
+            "http://corndog.io/",
+            "https://mondrianandme.com/",
+            "https://puginarug.com",
+            "https://alwaysjudgeabookbyitscover.com",
+            "https://thatsthefinger.com/",
+            "https://cant-not-tweet-this.com/",
+            "https://weirdorconfusing.com/",
+            "http://eelslap.com/",
+            "http://www.staggeringbeauty.com/",
+            "http://burymewithmymoney.com/",
+            "https://smashthewalls.com/",
+            "https://jacksonpollock.org/",
+            "http://endless.horse/",
+            "https://www.trypap.com/",
+            "http://www.republiquedesmangues.fr/",
+            "http://www.movenowthinklater.com/",
+            "http://www.partridgegetslucky.com/",
+            "http://www.rrrgggbbb.com/",
+            "http://beesbeesbees.com/",
+            "http://www.koalastothemax.com/",
+            "http://www.everydayim.com/",
+            "http://randomcolour.com/",
+            "http://cat-bounce.com/",
+            "http://chrismckenzie.com/",
+            "https://thezen.zone/",
+            "http://hasthelargehadroncolliderdestroyedtheworldyet.com/",
+            "http://ninjaflex.com/",
+            "http://ihasabucket.com/",
+            "http://corndogoncorndog.com/",
+            "http://www.hackertyper.com/",
+            "https://pointerpointer.com",
+            "http://imaninja.com/",
+            "http://drawing.garden/",
+            "http://www.ismycomputeron.com/",
+            "http://www.nullingthevoid.com/",
+            "http://www.muchbetterthanthis.com/",
+            "http://www.yesnoif.com/",
+            "http://lacquerlacquer.com",
+            "http://potatoortomato.com/",
+            "http://iamawesome.com/",
+            "https://strobe.cool/",
+            "http://www.pleaselike.com/",
+            "http://crouton.net/",
+            "http://corgiorgy.com/",
+            "http://www.wutdafuk.com/",
+            "http://unicodesnowmanforyou.com/",
+            "http://chillestmonkey.com/",
+            "http://scroll-o-meter.club/",
+            "http://www.crossdivisions.com/",
+            "http://tencents.info/",
+            "https://boringboringboring.com/",
+            "http://www.patience-is-a-virtue.org/",
+            "http://pixelsfighting.com/",
+            "http://isitwhite.com/",
+            "https://existentialcrisis.com/",
+            "http://onemillionlols.com/",
+            "http://www.omfgdogs.com/",
+            "http://oct82.com/",
+            "http://chihuahuaspin.com/",
+            "http://www.blankwindows.com/",
+            "http://dogs.are.the.most.moe/",
+            "http://tunnelsnakes.com/",
+            "http://www.trashloop.com/",
+            "http://www.ascii-middle-finger.com/",
+            "http://spaceis.cool/",
+            "http://www.donothingfor2minutes.com/",
+            "http://buildshruggie.com/",
+            "http://buzzybuzz.biz/",
+            "http://yeahlemons.com/",
+            "http://wowenwilsonquiz.com",
+            "https://thepigeon.org/",
+            "http://notdayoftheweek.com/",
+            "http://www.amialright.com/",
+            "http://nooooooooooooooo.com/",
+            "https://greatbignothing.com/",
+            "https://zoomquilt.org/",
+            "https://dadlaughbutton.com/",
+            "https://www.bouncingdvdlogo.com/",
+            "https://remoji.com/",
+            "http://papertoilet.com/"
+        };
+    }
+}
