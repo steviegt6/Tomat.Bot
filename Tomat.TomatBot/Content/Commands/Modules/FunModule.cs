1224c3d2c8b3540ccdfcc924c0906802c8db37b0
Stevie <xxlennygamerxx@gmail.com>
2021-09-24 23:53:16 -0700

    Fix parameter display.

--- Tomat.TomatBot/Content/Commands/Modules/FunModule.cs
+++ Tomat.TomatBot/Content/Commands/Modules/FunModule.cs
@@ -35,7 +35,7 @@ namespace Tomat.TomatBot.Content.Commands.Modules
         [Command("temp")]
         [Alias("temperature", "converttemp", "tempconvert", "convert")]
         [Summary("Converts temperatures to: fahrenheit, celsius, kelvin, rankine, and réaumur!")]
-        [Parameters("<temperature> (symbol suffix)")]
+        [Parameters("<temperature><symbol suffix>")]
         [RequireBotPermission(ChannelPermission.SendMessages)]
         public async Task ConvertTepAsync(string temp)
         {
