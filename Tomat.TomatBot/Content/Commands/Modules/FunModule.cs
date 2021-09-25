798c636f8909070b826fc69109346ecbe81c60ad
Stevie <xxlennygamerxx@gmail.com>
2021-09-25 00:01:48 -0700

    Random website command.

--- Tomat.TomatBot/Content/Commands/Modules/FunModule.cs
+++ Tomat.TomatBot/Content/Commands/Modules/FunModule.cs
@@ -12,6 +12,7 @@ using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 using Tomat.TomatBot.Common.Embeds;
+using Tomat.TomatBot.Common.Web.EitherIO;
 using Tomat.TomatBot.Content.Commands.Modules.Temperature;
 using Tomat.TomatBot.Core.CommandContext;
 using Tomat.TomatBot.Core.Services.Commands;
@@ -92,6 +93,151 @@ namespace Tomat.TomatBot.Content.Commands.Modules
 
 
             return builder.ToString();
+        }
+
+        #endregion
+
+        #region Either Command
+
+
+        [Command("either")]
+        [Alias("wyr", "wouldyourather")]
+        [Summary("Displays a Would-You-Rather question taken directly from [either.io](http://either.io/).")]
+        [RequireBotPermission(ChannelPermission.SendMessages)]
+        public async Task EitherAsync()
+        {
+            EitherIORequest options = EitherIORequest.MakeRequest();
+
+            if (!string.IsNullOrEmpty(options.Exception))
+            {
+                Embed embed = EmbedHelper.ErrorEmbed(
+                    "Error while attempting to make a request to [either.io](http://either.io/)." +
+                    "\nIf the website is up and this issue persists, contact a developer." +
+                    $"\n\nException: {options.Exception}",
+                    new BaseEmbed(Context.Bot, Context.User).Footer);
+                await ReplyAsync(embed: embed);
+                return;
+            }
+
+            BaseEmbed realEmbed = new(Context.Bot, Context.User)
+            {
+                Title = "Would you rather...",
+
+                Description = $":one: {options.Options.OptionOne}" +
+                              $"\n:two: {options.Options.OptionTwo}"
+            };
+
+            IUserMessage? message = await ReplyAsync(embed: realEmbed.Build());
+            await message.AddReactionAsync(new Emoji("1️⃣"));
+            await message.AddReactionAsync(new Emoji("2️⃣"));
+        }
+
+        #endregion
+
+        #region Randomization
+
+        public static readonly string[] Websites =
+{
+            "https://longdogechallenge.com/",
+            "http://heeeeeeeey.com/",
+            "http://corndog.io/",
+            "https://mondrianandme.com/",
+            "https://puginarug.com",
+            "https://alwaysjudgeabookbyitscover.com",
+            "https://thatsthefinger.com/",
+            "https://cant-not-tweet-this.com/",
+            "https://weirdorconfusing.com/",
+            "http://eelslap.com/",
+            "http://www.staggeringbeauty.com/",
+            "http://burymewithmymoney.com/",
+            "https://smashthewalls.com/",
+            "https://jacksonpollock.org/",
+            "http://endless.horse/",
+            "https://www.trypap.com/",
+            "http://www.republiquedesmangues.fr/",
+            "http://www.movenowthinklater.com/",
+            "http://www.partridgegetslucky.com/",
+            "http://www.rrrgggbbb.com/",
+            "http://beesbeesbees.com/",
+            "http://www.koalastothemax.com/",
+            "http://www.everydayim.com/",
+            "http://randomcolour.com/",
+            "http://cat-bounce.com/",
+            "http://chrismckenzie.com/",
+            "https://thezen.zone/",
+            "http://hasthelargehadroncolliderdestroyedtheworldyet.com/",
+            "http://ninjaflex.com/",
+            "http://ihasabucket.com/",
+            "http://corndogoncorndog.com/",
+            "http://www.hackertyper.com/",
+            "https://pointerpointer.com",
+            "http://imaninja.com/",
+            "http://drawing.garden/",
+            "http://www.ismycomputeron.com/",
+            "http://www.nullingthevoid.com/",
+            "http://www.muchbetterthanthis.com/",
+            "http://www.yesnoif.com/",
+            "http://lacquerlacquer.com",
+            "http://potatoortomato.com/",
+            "http://iamawesome.com/",
+            "https://strobe.cool/",
+            "http://www.pleaselike.com/",
+            "http://crouton.net/",
+            "http://corgiorgy.com/",
+            "http://www.wutdafuk.com/",
+            "http://unicodesnowmanforyou.com/",
+            "http://chillestmonkey.com/",
+            "http://scroll-o-meter.club/",
+            "http://www.crossdivisions.com/",
+            "http://tencents.info/",
+            "https://boringboringboring.com/",
+            "http://www.patience-is-a-virtue.org/",
+            "http://pixelsfighting.com/",
+            "http://isitwhite.com/",
+            "https://existentialcrisis.com/",
+            "http://onemillionlols.com/",
+            "http://www.omfgdogs.com/",
+            "http://oct82.com/",
+            "http://chihuahuaspin.com/",
+            "http://www.blankwindows.com/",
+            "http://dogs.are.the.most.moe/",
+            "http://tunnelsnakes.com/",
+            "http://www.trashloop.com/",
+            "http://www.ascii-middle-finger.com/",
+            "http://spaceis.cool/",
+            "http://www.donothingfor2minutes.com/",
+            "http://buildshruggie.com/",
+            "http://buzzybuzz.biz/",
+            "http://yeahlemons.com/",
+            "http://wowenwilsonquiz.com",
+            "https://thepigeon.org/",
+            "http://notdayoftheweek.com/",
+            "http://www.amialright.com/",
+            "http://nooooooooooooooo.com/",
+            "https://greatbignothing.com/",
+            "https://zoomquilt.org/",
+            "https://dadlaughbutton.com/",
+            "https://www.bouncingdvdlogo.com/",
+            "https://remoji.com/",
+            "http://papertoilet.com/"
+        };
+
+        [Command("randomwebsite")]
+        [Alias("randweb", "ranweb", "rw")]
+        [Summary("Links to a random website from a list provided by [TheUselessWeb](https://theuselessweb.com/).")]
+        [RequireBotPermission(ChannelPermission.SendMessages)]
+        public async Task RandomWebsiteAsync()
+        {
+            BaseEmbed embed = new(Context.Bot, Context.User)
+            {
+                Title = "Random Useless Website",
+
+                Description = $"Click [here]({Websites[new Random().Next(Websites.Length)]}) to go to a random, useless website." +
+                              $"\nWant another one? Run this command again." +
+                              "\n\nWebsites taken from [TheUselessWeb](https://theuselessweb.com/)."
+            };
+
+            await ReplyAsync(embed: embed.Build());
         }
 
         #endregion
